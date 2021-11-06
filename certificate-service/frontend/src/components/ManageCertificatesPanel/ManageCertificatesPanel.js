8c43a4b8ba02d91566b377a1de7d6e8af2351895
Andrea Tabone <andrea.tabone@broadcom.com>
2021-11-06 16:14:27 +0100

    Wip - get pem certificate
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- certificate-service/frontend/src/components/ManageCertificatesPanel/ManageCertificatesPanel.js
+++ certificate-service/frontend/src/components/ManageCertificatesPanel/ManageCertificatesPanel.js
@@ -2,11 +2,20 @@ import React, { useState, useMemo } from "react";
 import { render } from "react-dom";
 import Styles from "./Style";
 import { useForm, useField } from "react-final-form-hooks";
+import LoadingButton from '@mui/lab/LoadingButton';
+import {Box, FormControlLabel, Switch} from "@material-ui/core";
+import {TextareaAutosize} from "@mui/material";
 
 const ManageCertificatesPanel = () => {
     const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
     const [pemCert, setPemCert] = React.useState(null);
-
+    const [serviceUrl, setServiceUrl] = React.useState(null);
+    const [certAlias, setCertAlias] = useState(null);
+    const [errors, setErrors] = React.useState(null);
+    const [loading, setLoading] = React.useState(false);
+    function handleClick() {
+        setLoading(true);
+    }
     const onSubmit = async values => {
         await sleep(300);
         window.alert(JSON.stringify(values, 0, 2));
@@ -23,18 +32,22 @@ const ManageCertificatesPanel = () => {
 
     const validate = values => {
         const errors = {};
+        setCertAlias(values.alias);
+        setServiceUrl(values.url);
+
         if (!values.url) {
             errors.url = "Required";
         }
-        if (!values.lastName) {
-            errors.lastName = "Required";
+        if (!values.alias) {
+            errors.alias = "Required";
         }
         return errors;
     };
 
     const initialValues = useMemo(
         () => ({
-            url: "https://localhost:8080",
+            alias: "",
+            url: "",
             certificate: ""
         }),
         []
@@ -42,21 +55,23 @@ const ManageCertificatesPanel = () => {
 
     // TODO get list of certificate
 
-    // const getCertificateInPemFormat = () => {
-    //     const url = process.env.REACT_APP_GATEWAY_URL + `/api/v1/certificate-service/certificate?url=${serviceUrl}`
-    //     fetch(url, {
-    //         method: 'GET',
-    //         mode: 'cors'
-    //     }).then((response) => {
-    //         if (!response.ok) {
-    //             throw Error(response.statusText);
-    //         }
-    //         setPemCert(response.json())
-    //     }).catch((error) => {
-    //         console.log(error)
-    //         setErrors(error.message);
-    //     });
-    // }
+    const getCertificateInPemFormat = async () => {
+        const url = process.env.REACT_APP_GATEWAY_URL + `/api/v1/certificate-service/certificate?url=${serviceUrl}`
+        fetch(url, {
+            method: 'GET',
+            mode: 'cors'
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.text();
+        }).then(result => {
+            setPemCert(result);
+        }).catch((error) => {
+            console.log(error)
+            setErrors(error.message);
+        });
+    }
 
     const { form, handleSubmit, values, pristine, submitting } = useForm({
         onSubmit,
@@ -65,6 +80,7 @@ const ManageCertificatesPanel = () => {
     });
 
     const alias = useField("alias", form);
+    const url = useField("url", form);
     const certificate = useField("certificate", form);
 
     const removeCertificate = (name) => {
@@ -95,6 +111,42 @@ const ManageCertificatesPanel = () => {
                 </tr>
             </table>
             <form>
+                <Box sx={{ '& > button': { m: 1 } }}>
+                    <FormControlLabel
+                        sx={{
+                            display: 'block',
+                        }}
+                        control={
+                            <Switch
+                                checked={loading}
+                                onChange={() => setLoading(!loading)}
+                                name="loading"
+                                color="primary"
+                            />
+                        }
+                        label="Retrieve certificate"
+                    />
+                </Box>
+                <div>
+                    <label disabled={!loading} >Hostname</label>
+                    <input disabled={!loading} {...url.input} placeholder="Service Hostname" />
+                    {url.meta.touched && url.meta.error && (
+                        <span>{url.meta.error}</span>
+                    )}
+                </div>
+                <div className="buttons">
+                    <button disabled={!loading} type="submit" onClick={getCertificateInPemFormat}>
+                        Get certificate
+                    </button>
+                </div>
+                { pemCert !== null && (
+                    <TextareaAutosize
+                        aria-label="minimum height"
+                        minRows={3}
+                        defaultValue={pemCert}
+                        style={{ width: 200 }}
+                    />
+                )}
                 <div>
                     <label >Alias</label>
                     <input {...alias.input} placeholder="Certificate Alias" />
@@ -102,7 +154,6 @@ const ManageCertificatesPanel = () => {
                         <span>{alias.meta.error}</span>
                     )}
                 </div>
-
                 <div>
                     <label>Certificate to use</label>
                     <input {...certificate.input} type="textarea" placeholder="Provide base64 encoded certificate." />
